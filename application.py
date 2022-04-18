cf5c1198d5659215658c808fed5fcbd636f75b6c
Iqra S Rao <iqra.saadia@gmail.com>
2022-04-18 16:58:47 +0800

    Update application.py

--- application.py
+++ application.py
@@ -3,10 +3,5 @@ application = Flask(__name__)
 app = application
 
 @application.route('/')
-@application.route('/index/')
 def hello():
     return '<h1>Hello, World!</h1>'
-
-@application.route('/about/')
-def about():
-    return '<h3>Moovbot.</h3>'
